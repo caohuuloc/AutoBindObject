22d3c1e93ac239229555c6cc3423481ddd691d39
Cao Huu Loc <caohuuloc@yahoo.com>
2016-11-08 15:02:37 +0700

    Creating Pod lib

---
+++ Example/Pods/Target Support Files/Pods-AutoBindObject_Example/Pods-AutoBindObject_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AutoBindObject_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AutoBindObject_ExampleVersionString[];
+
